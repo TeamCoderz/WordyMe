b09506fee50016bcb2fdafbb16171160d91aa5f7
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-12-08 16:45:06 +0200

    feat(backend): integrate better-auth for authentication and add user schema

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,9 +1,14 @@
 import express, { type Express } from 'express';
 import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth.js';
 
 const app: Express = express();
 
 app.use(cors());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
---
+++ apps/backend/src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { bearer, openAPI } from "better-auth/plugins"
+import { db } from './db.js';
+
+export const adapter = drizzleAdapter(
+    db,
+    {
+        provider: "sqlite",
+        usePlural: true,
+    }
+);
+
+export const auth = betterAuth({
+    database: adapter,
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [bearer(), openAPI()],
+});
