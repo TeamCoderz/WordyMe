581c001f3c3cd7cf5541175cc546bacace9fb819
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-12-14 15:27:27 +0200

    feat: add ESLint configuration

--- apps/backend/src/middlewares/errors.ts
+++ apps/backend/src/middlewares/errors.ts
@@ -2,7 +2,7 @@ import { ErrorRequestHandler, RequestHandler } from "express";
 import { toHttpException } from "../utils/errors.js";
 import { HttpNotFound } from "@httpx/exception";
 
-export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
     console.error(err);
 
     const httpException = toHttpException(err);
@@ -10,6 +10,6 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     res.status(httpException.statusCode).json(httpException);
 };
 
-export const notFoundHandler: RequestHandler = (req, res, next) => {
+export const notFoundHandler: RequestHandler = () => {
     throw new HttpNotFound();
 };
